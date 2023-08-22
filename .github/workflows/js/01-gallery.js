6a229bcf848b55b08c5482ec8f968de1ce736374
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-08-22 21:49:55 +0400

    hw-8

---
+++ .github/workflows/js/01-gallery.js
@@ -0,0 +1,38 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox"; 
+console.log(galleryItems);
+
+
+console.log(galleryItems);
+function createGalleryMarkup(galleryObj) {
+  return galleryObj
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          width = "700"
+          
+        />
+      </a>
+    </li>`
+    )
+    .join("");
+}
+
+const galleryRef = document.querySelector(".gallery");
+galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
