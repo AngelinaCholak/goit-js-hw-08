e5336a773d6a0e08cfd16d17f7eb051a7d30f438
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-08-24 19:14:05 +0400

    hw-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player.Player(iframe);
+
+player.on('timeupdate', throttle(saveCurrentBrowsingTime, 1000));
+
+function saveCurrentBrowsingTime(event) {
+    localStorage.setItem(
+        'video-current-time',
+        JSON.stringify(event, seconds)
+    );
+}
+try {
+    const savedTime = JSON.parse(
+        localStorage.getItem('video-current-time')
+    );
+
+    if (savedTime) {
+        player.saveCurrentTime(savedTime);
+    }
+} catch (error) {
+        console.error(error.name, error.message);
+    }
