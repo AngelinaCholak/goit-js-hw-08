1ac8502b1967358e1b2ac02d591635c0d0ac533d
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-08-26 21:41:35 +0400

    hw-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,24 +2,25 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
-const player = new Player.Player(iframe);
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(saveCurrentBrowsingTime, 1000));
 
 function saveCurrentBrowsingTime(event) {
     localStorage.setItem(
-        'video-current-time',
-        JSON.stringify(event, seconds)
+        'videoplayer-current-time',
+        JSON.stringify(event.seconds)
     );
 }
+
 try {
     const savedTime = JSON.parse(
-        localStorage.getItem('video-current-time')
+        localStorage.getItem('videoplayer-current-time')
     );
 
-    if (savedTime) {
-        player.saveCurrentTime(savedTime);
+    if (savedTime !== null) {
+        player.setCurrentTime(savedTime);
     }
 } catch (error) {
-        console.error(error.name, error.message);
-    }
+    console.error(error.name, error.message);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+const _ = require('lodash');
 
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const form = document.querySelector('.feedback-form');
+
+let feedbackFromState = {
+    email: '',
+    message: ''
+};
+const saveСurrentValue = _.throttle(() => {
+    feedbackFromState.email = emailInput.value;
+    feedbackFromState.message = messageInput.value;
+
+    localStorage.setItem(
+        'feedback-form-state',
+        JSON.stringify(feedbackFromState)
+    );
+}, 500);
+// Під час завантаження сторінки перевіряй стан сховища,
+//  і якщо там є збережені дані, заповнюй ними поля форми.
+//  В іншому випадку поля повинні бути порожніми.
+
+document.querySelector('DOMContentLoaded', function () {
+    const storageStatus = localStorage.getItem('feedback-form-state');
+    if (storageStatus) {
+        feedbackFromState = JSON.parse(storageStatus);
+        emailInput.value = feedbackFromState.email;
+        messageInput.value = feedbackFromState.message;
+
+    }
+});
+
+emailInput.addEventListener('input', saveСurrentValue);
+messageInput.addEventListener('input', saveСurrentValue);
+
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+     
+    console.log({
+    email: feedbackFromState.email,
+    message: feedbackFromState.message
+  });
+    
+});
