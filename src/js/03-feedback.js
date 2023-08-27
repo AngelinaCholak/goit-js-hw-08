240eb9679da528b0ac6b7b8fc63ef226e1bb71fb
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-08-27 13:55:27 +0400

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,51 +1,55 @@
 const _ = require('lodash');
 
+
 const emailInput = document.querySelector('input[name="email"]');
 const messageInput = document.querySelector('textarea[name="message"]');
 const form = document.querySelector('.feedback-form');
 
-let feedbackFromState = {
+let feedbackFormState = {
     email: '',
     message: ''
 };
-const saveСurrentValue = _.throttle(() => {
-    feedbackFromState.email = emailInput.value;
-    feedbackFromState.message = messageInput.value;
 
-    localStorage.setItem(
-        'feedback-form-state',
-        JSON.stringify(feedbackFromState)
-    );
-}, 500);
-// Під час завантаження сторінки перевіряй стан сховища,
-//  і якщо там є збережені дані, заповнюй ними поля форми.
-//  В іншому випадку поля повинні бути порожніми.
 
-document.querySelector('DOMContentLoaded', function () {
-    const storageStatus = localStorage.getItem('feedback-form-state');
-    if (storageStatus) {
-        feedbackFromState = JSON.parse(storageStatus);
-        emailInput.value = feedbackFromState.email;
-        messageInput.value = feedbackFromState.message;
+const updateFormStateThrottled = _.throttle(() => {
+    feedbackFormState.email = emailInput.value;
+    feedbackFormState.message = messageInput.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormState));
+}, 500); 
 
+emailInput.addEventListener('input', updateFormStateThrottled);
+messageInput.addEventListener('input', updateFormStateThrottled);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const savedState = localStorage.getItem('feedback-form-state');
+    if (savedState) {
+        feedbackFormState = JSON.parse(savedState);
+        emailInput.value = feedbackFormState.email;
+        messageInput.value = feedbackFormState.message;
     }
 });
 
-emailInput.addEventListener('input', saveСurrentValue);
-messageInput.addEventListener('input', saveСurrentValue);
-
-
 
 form.addEventListener('submit', function (event) {
-    event.preventDefault()
-    
-    localStorage.removeItem('feedback-form-state');
-    emailInput.value = '';
-    messageInput.value = '';
-     
-    console.log({
-    email: feedbackFromState.email,
-    message: feedbackFromState.message
-  });
-    
+    event.preventDefault();
+
+    if (feedbackFormState.email && feedbackFormState.message) {
+        console.log({
+            email: feedbackFormState.email,
+            message: feedbackFormState.message
+        });
+
+        localStorage.removeItem('feedback-form-state');
+        emailInput.value = '';
+        messageInput.value = '';
+    } else {
+        console.log("Both fields must be filled out before submitting.");
+    }
+});
+
+
+window.addEventListener('beforeunload', function () {
+    updateFormStateThrottled.cancel(); 
+    updateFormStateThrottled(); 
 });
